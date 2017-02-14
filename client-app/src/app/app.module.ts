d50659b0f7e7709387cddce2d688a42421d8ba36
zunelewin <zunelewin@gmail.com>
2017-02-14 15:54:14 +0000

    start client Angular 2 app

---
+++ client-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { CommentComponent } from './comment/comment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CommentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
